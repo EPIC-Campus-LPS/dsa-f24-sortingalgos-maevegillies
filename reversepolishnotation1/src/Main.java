db4e7de82cfdc276ff38d039bd5030a7cd62e6b1
Maeve Gillies <143544659+maevegillies@users.noreply.github.com>
2024-12-11 21:26:08 -0700

    Delete reversepolishnotation1 directory

--- reversepolishnotation1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //takes in input
-        //runs the method
-        //print return value
-//        System.out.println(ReversePolishNotation.evaluatePostfix("7 11 + 9 / 3 5 * -"));
-        System.out.println(ReversePolishNotation.infixToPostFix("( a + ( b * c ) ) + ( d  * e + f ) * g"));
-        }
-    }
